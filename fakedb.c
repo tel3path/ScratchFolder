abb9d5222c66314fe4917c01bb382e45424c8d2e
H Oliver <helen.oliver@bcs.org.uk>
2022-11-14 18:38:14 +0000

    Add files via upload
    Removed the money struct as it isn't being used

--- fakedb.c
+++ fakedb.c
@@ -1,5 +1,5 @@
 // Comment this out if compiling in a non-CHERI environment
-#include "include/common.h"
+//#include "include/common.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,8 +9,6 @@
 
 int main(int argc, char *argv[])
 {
-    struct money *g6 = setMoney(30000.00, 35);
-
     // this shouldn't crash
     char *eid;
     eid = (char*)malloc(sizeof(char));
@@ -23,9 +21,9 @@ int main(int argc, char *argv[])
     int emid = 0;
     char esd[11];
     strcpy(esd, "2020-06-30");
-    struct employee *e1 = setEmployee(eid, enm, ejt, emid, esd, g6); 
+    struct employee *e1 = setEmployee(eid, enm, ejt, emid, esd); 
     printf("This should look right:\n\n");
-    printEmployee(e1, true);
+    printEmployee(e1);
     free(eid);
     
     // hopefully this *should* crash
@@ -34,15 +32,13 @@ int main(int argc, char *argv[])
     free(eid);    
     e1->empId = *eid;
     printf("If it hasn't crashed, this should at least look wrong:\n\n");
-    printEmployee(e1, true);
+    printEmployee(e1);
 
     enm = malloc(101);
     strcpy(enm, "123456789101112ladybugscametotheladybugs\'picnic");
     printf("On Apple clang it falls over here, with \"illegal hardware instruction\"\n");
     strcpy(e1->empName, enm);
-    strcpy(ejt, "Scrivener");
-    strcpy(esd, "2020-06-30");
-    printEmployee(e1, true);
+    printEmployee(e1);
     free(e1);
     printf("If I compile and run this on Debian g++ it runs to the end (here)\n");
 
@@ -53,52 +49,24 @@ int main(int argc, char *argv[])
 * Takes in every member of an employee struct, instantiates it,
 * sets the values, and returns it
 */
-struct employee * setEmployee(char *eid, char enm[101], char ejt[101], int emgr, char esd[11],
-struct money *em) {
+struct employee * setEmployee(char *eid, char enm[101], char ejt[101], int emgr, char esd[11]) {
     struct employee *e = malloc(sizeof (struct employee));
     e->empId = *eid;
     strcpy(e->empName, enm);
     strcpy(e->jobTitle, ejt);
     e->mgrId = emgr;
     strcpy(e->startDate, esd);
-    e->salary = *em;
     return e;
 }
 
-/**
-* Takes in every member of a money struct, instantiates it,
-* sets the values, and returns it
-*/
-struct money * setMoney(long wd, unsigned char fd) {
-    struct money *m = malloc(sizeof (struct money));
-    m->whole_dollars = wd;
-    m->frac_dollar = fd;
-    return m;
-}
-
-void printEmployee(struct employee *emp, bool bStandalone) {
-    if(bStandalone) {
-        printf("EMPLOYEE\n");
-        printf("========\n");
-    }
+void printEmployee(struct employee *emp) {
+    printf("EMPLOYEE\n");
+    printf("========\n");
     
     printf("Employee id is %c\n", emp->empId);
     printf("Employee is %s\n", emp->empName);
     printf("Employee job title is %s\n", emp->jobTitle);
     printf("Employee manager id is %d\n", emp->mgrId);
-    printf("Employee start date was %s\n", emp->startDate);
-    printf("Employee salary is ");
-    printMoney(&emp->salary);
-    printf("\n");
-    
-    if(bStandalone)
-        printf("\n");
-}
-
-/**
-* if only
-*/
-void printMoney(struct money *m) {
-    printf("%ld.%d", m->whole_dollars, m->frac_dollar);
+    printf("Employee start date was %s\n\n", emp->startDate);
 }
 
--- fakedb.h
+++ fakedb.h
@@ -1,23 +1,7 @@
 // fakedb.h
 
-/**
-* Because it doesn't work to store money as
-* a float or decimal.
-*/
-struct money {
-    // future-proof for inflation
-    // all currency is $
-    long whole_dollars; 
-    // cents
-    unsigned char frac_dollar; 
-};
-
-/**
-* A minimal employee 
-*/
 struct employee {
     // employee ID number
-    // (Let's make this a char. Not recommended for global megacorps)
     char empId;
     // employee name
     char empName[10];
@@ -28,15 +12,9 @@ struct employee {
     // Just use a string for now, time structures TODO
     // YYYY-MM-DD
     char startDate[11];
-    // not a grade or spine point or anything, just an amount TODO
-    struct money salary;
 };
 
-void printEmployee(struct employee *emp, bool bStandalone);
+void printEmployee(struct employee *emp);
 
-void printMoney(struct money * m);
+struct employee * setEmployee(char *eid, char enm[10], char ejt[101], int emgr, char sd[11]);
 
-struct employee * setEmployee(char *eid, char enm[10], char ejt[101], int emgr, char sd[11],
-struct money *em);
-
-struct money * setMoney(long wd, unsigned char fd);
