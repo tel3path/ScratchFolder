c8a13cd6633761fda9daf7c23ea16c231c1a1424
H Oliver <helen.oliver@bcs.org.uk>
2022-11-16 16:59:02 +0000

    Add files via upload
    Now only shows the length.

--- cheripointerbounds.c
+++ cheripointerbounds.c
@@ -14,7 +14,6 @@ int main(int argc, char *argv[]) {
     int bounds[2] = {lowertest, highertest};
     
     for(int i=0; i<sizeof(bounds); i++) {
-        printf("Using malloc to give %d bytes to buffer.\n", bounds[i]);
         char * buffer = malloc(bounds[i]);
         if( buffer==NULL )
         {
@@ -24,13 +23,9 @@ int main(int argc, char *argv[]) {
         uint64_t length = cheri_length_get(buffer);
         printf("Length of buffer is: %lu\n", length);
         uint64_t base = cheri_base_get(buffer);
-        printf("Lower bound of buffer is %04lx\n", base);
         uint64_t ubound = base + length;
-        printf("Upper bound of buffer is %04lx\n", ubound);
 
-        printf("About to free this buffer.\n");
         free(buffer);
-        printf("Successfully freed the buffer.\n");
     }
 }
 
